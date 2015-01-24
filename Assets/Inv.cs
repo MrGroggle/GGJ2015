fd846e1ec696478f4b4c9338ba8564d335e66ed2
Alex <alex_jamesclark@hotmail.co.uk>
2015-01-24 14:43:08 +0000

    Re-added arms and changed scripts
    Put the arms back into the scene
    Changed the JS files to Cs

---
+++ Assets/Inv.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Inv : MonoBehaviour {
+	public GUISkin menuSkin;
+	public int wood= 0;
+	public int fish = 0;
+	public int water= 0;
+	public int bandage = 0;
+	public int food = 0;
+	public int minimumVal = 0;
+	private bool showGUI = false;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(wood <= 0)
+		{
+			wood = minimumVal;
+		}
+		
+		if(fish <= 0)
+		{
+			fish = minimumVal;
+		}
+		
+		if(food <= 0)
+		{
+			food = minimumVal;
+		}
+		
+		if(water <= 0)
+		{
+			water = minimumVal;
+		}
+		
+		if(bandage <= 0)
+		{
+			bandage = minimumVal;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.I))
+		{
+			showGUI = !showGUI;
+			
+		}
+
+		if ( showGUI == true)
+		{
+			Time.timeScale = 0;
+			GameObject.Find("First Person Controller").GetComponent<FPSInputController>().enabled = false;
+			GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled = false;
+			GameObject.Find("Main Camera").GetComponent<MouseLook>().enabled = false;
+			GameObject.Find("FPSArms_Axe@Idle").GetComponent<playerControl>().enabled = false;
+			
+		} 	
+		
+		if ( showGUI == false)
+		{
+			Time.timeScale = 1;
+			GameObject.Find("First Person Controller").GetComponent<FPSInputController>().enabled = true;
+			GameObject.Find("First Person Controller").GetComponent<MouseLook>().enabled = true;
+			GameObject.Find("Main Camera").GetComponent<MouseLook>().enabled = true;
+			GameObject.Find("FPSArms_Axe@Idle").GetComponent<playerControl>().enabled = true;
+			
+		} 
+	}
+
+
+	void OnGUI()
+	{
+		if (showGUI == true)
+		{
+			GUI.skin = menuSkin;
+			GUI.BeginGroup(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 150, 300, 300));
+			GUI.Box(new Rect(0, 0, 300, 300), "Basic Inventory");
+
+			//get wood! ;)
+			GUI.Label(new Rect(10, 50, 50, 50), "Wood");
+			GUI.Box(new Rect(60, 50, 20, 20), "" + wood);
+
+			//more holders for items in game
+			GUI.Label(new Rect(10, 130, 50, 50), "Fish");
+			GUI.Box(new Rect(60, 130, 20, 20), "" + fish);
+			
+			//Edable items
+			GUI.Label(new Rect(10, 190, 50, 50), "Food");
+			GUI.Box(new Rect(60, 190, 20, 20), "" + food);
+			if(GUI.Button(new Rect(100, 190, 100, 20), "Eat Food?"))
+			{
+				if(food >= 1)
+				{
+					food--;
+					Eat();
+				}
+			}
+			
+			GUI.Label(new Rect(10, 210, 50, 50), "Water");
+			GUI.Box(new Rect(60, 210, 20, 20), "" + water);
+			if(GUI.Button(new Rect(100, 210, 100, 20), "Drink Water?"))
+			{
+				if(water >= 1)
+				{
+					water--;
+					Drink();
+				}
+			}
+			
+			GUI.EndGroup();
+		}
+		
+	}
+	void Eat()
+	{
+		//playerGUI.hungerBarDisplay += 0.1;
+	}
+	
+	void Drink()
+	{
+		//playerGUI.thirstBarDisplay += 0.1;
+	}
+
+}
---
+++ Assets/playerControl.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+
+	private bool  hasAxe = false;
+	private bool canSwing = true;
+	private bool isSwinging = false;
+	private CharacterController controller;
+	//private PlayerGUI playerGUI;
+	public float swingTimer = 0.7f;
+
+	void Start()
+	{
+		hasAxe = true;
+		controller = GameObject.Find ("First Person Controller").GetComponent<CharacterController> ();
+		// implement player gui 
+		//playerGUI = GameObject.Find("First Person Controller").GetComponent<PlayerGUI>();
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		//If we aren't moving and if we aren't swinging, then we idle!
+		
+		if(controller.velocity.magnitude <= 0 && isSwinging == false)
+		{
+			animation.Play("Idle");
+			animation["Idle"].wrapMode = WrapMode.Loop;
+			animation["Idle"].speed = 0.2f;
+		}
+		
+		//If we're holding shift and moving, then sprint!
+		
+		if(controller.velocity.magnitude > 0 && Input.GetKey(KeyCode.LeftShift))
+		{
+			animation.Play("Sprint");
+			animation["Sprint"].wrapMode = WrapMode.Loop;
+		}
+		
+		if(hasAxe == true && canSwing == true)
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				//Stamina reduction applied to the PlayerGUI script
+				//playerGUI.staminaBarDisplay -= 0.1;
+				
+				//Swinging animation
+				animation.Play("Swing");
+				animation["Swing"].speed = 2;
+				isSwinging = true;
+				canSwing = false;
+			}
+		}
+		
+		if(canSwing == false)
+		{
+			swingTimer -= Time.deltaTime;
+		}
+		
+		if(swingTimer <= 0)
+		{
+			swingTimer = 1;
+			canSwing = true;
+			isSwinging = false;
+		}
+	}
+}
