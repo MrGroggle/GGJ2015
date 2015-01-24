07b46a6d81b7545cdb4940415560a70111030d6b
MrGroggle <sempleharry@gmail.com>
2015-01-24 14:01:01 +0000

    Added GUI Interface in Visual Basic to catch killer's IP address
    Gui implemented, jumping not working, moved some core files over to CS
    from JS

--- Assets/PlayerGUI.cs
+++ Assets/PlayerGUI.cs
@@ -1,50 +1,157 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 //Size of textures
 
 
-public class PlayerGUI : MonoBehaviour {
+public class PlayerGUI : MonoBehaviour {	
+	
+	public Vector2 barSize = new Vector2(240, 20);
+	
+	//Health
+	public float healthFallRate = 150f;
+
+	public Vector2 healthPos = new Vector2(20,20);
+	public float healthDisplay = 1f;
+	public Texture2D healthBarEmpty;
+	public Texture2D healthBarFull;
+	
+	//Hunger
+	public float hungerFallRate = 150f;
+
+	public Vector2 hungerPos = new Vector2(80,20);
+	public float hungerDisplay = 1f;
+	public Texture2D hungerBarEmpty;
+	public Texture2D hungerBarFull;
+	
+	//Stamina
+	public float staminaFallRate = 150f;
+	
+	public Vector2 staminaPos = new Vector2(80,20);
+	public float staminaDisplay = 1f;
+	public Texture2D staminaBarEmpty;
+	public Texture2D staminaBarFull;
+
+	
+	private CharacterMotor motor;
+	public CharacterController controller;
+	public bool canJump = true;
+	public float jumpTimer = 0.7f;
+	public float barLength = 0.0f;
 
 	// Use this for initialization
 	void Start () 
 	{
-		chMotor = GetComponent (CharacterMotor);
+		controller = GetComponent<CharacterController>();
+		motor = GetComponent<CharacterMotor>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		if (hungerDisplay <= 0) 
+		{
+			hungerDisplay = 0;
+			healthDisplay -= Time.deltaTime / healthFallRate;
+		}
+		if(healthDisplay <=0)
+		{
+			characterDeath();
+		}
+		if (hungerDisplay >= 0) 
+		{
+			hungerDisplay -= Time.deltaTime / hungerFallRate;
+		}
+		if (hungerDisplay >= 1) 
+		{
+			hungerDisplay = 1f;
+		}
+		if (staminaDisplay >= 1) 
+		{
+			staminaDisplay = 1f;
+		}
+		if (staminaDisplay <= 0) 
+		{
+			staminaDisplay = 0;
+			motor.movement.maxForwardSpeed = 6;
+			motor.movement.maxSidewaysSpeed = 6;
+		}
+		if (controller.velocity.magnitude > 0 && Input.GetButton("Sprint")) 
+		{
+			motor.movement.maxForwardSpeed = 10;
+			motor.movement.maxSidewaysSpeed = 10;
+			staminaDisplay -= Time.deltaTime / staminaFallRate * 1.5f;
+		} 
+		else 
+		{
+			motor.movement.maxForwardSpeed = 6;
+			motor.movement.maxSidewaysSpeed = 6;
+			staminaDisplay += Time.deltaTime / staminaFallRate;
+		}
+
+		if (Input.GetButton("Jump") && canJump == true) 
+		{
+			Debug.Log("Jumping");
+			staminaDisplay -= 0.2f;
+		}
+		if (!canJump)
+		{
+			jumpTimer = Time.deltaTime;
+			motor.jumping.enabled = false;
+		}
+		
+		if(jumpTimer <= 0)
+		{
+			canJump = true;
+			motor.jumping.enabled = true;
+			jumpTimer = 0.7f;
+		}
+		
+		if(staminaDisplay <= 0.2f)
+		{
+			canJump = false;
+			motor.jumping.enabled = false;
+		}	
+		
+
+
 	}
 	void OnGUI()
 	{
-		GUI.Box (new Rect (5, 30, 50, 23), "Health");
-		GUI.Box (new Rect (5, 55, 50, 23), "Hunger");
+		//Health
+		GUI.BeginGroup (new Rect (healthPos.x, healthPos.y, barSize.x, barSize.y));
+		GUI.Box (new Rect (0, 0, barSize.x, barSize.y), healthBarEmpty);
 
-		GUI.Box (new Rect (55, 30, barLength, 23), "PLACEHOLDER");
+		GUI.BeginGroup (new Rect (0, 0, barSize.x * healthDisplay, barSize.y));
+		GUI.Box (new Rect (0, 0, barSize.x, barSize.y), healthBarFull);
+
+		GUI.EndGroup ();
+		GUI.EndGroup ();
+
+		//Hunger
+		GUI.BeginGroup (new Rect (hungerPos.x, hungerPos.y, barSize.x, barSize.y));
+		GUI.Box (new Rect (0, 0, barSize.x, barSize.y), hungerBarEmpty);
+		
+		GUI.BeginGroup (new Rect (0, 0, barSize.x * hungerDisplay, barSize.y));
+		GUI.Box (new Rect (0, 0, barSize.x, barSize.y), hungerBarFull);
+		
+		GUI.EndGroup ();
+		GUI.EndGroup ();
+
+		//Stamina
+		GUI.BeginGroup (new Rect (staminaPos.x, staminaPos.y, barSize.x, barSize.y));
+		GUI.Box (new Rect (0, 0, barSize.x, barSize.y), staminaBarEmpty);
+		
+		GUI.BeginGroup (new Rect (0, 0, barSize.x * staminaDisplay, barSize.y));
+		GUI.Box (new Rect (0, 0, barSize.x, barSize.y), staminaBarFull);
+		
+		GUI.EndGroup ();
+		GUI.EndGroup ();
+	}
+	
+	void characterDeath()
+	{
+		Application.LoadLevel("SurvivalScene");
 	}
 
-
-	Vector2 barSize = new Vector2(240, 20);
-
-	//Health
-	Vector2 healthPos = new Vector2(20,20);
-	float healthDisplay = 1f;
-	Texture2D healthBarEmpty;
-	Texture2D healthBarFull;
-
-	//Hunger
-	Vector2 hungerPos = new Vector2(20,20);
-	float hungerDisplay = 1f;
-	Texture2D hungerBarEmpty;
-	Texture2D hungerBarFull;
-
-	float healthFallRate = 150f;
-	float hungerFallRate = 150f;
-
-	CharacterMotor chMotor;
-	CharacterController controller;
-	bool m_canJump = false;
-	float jumpTimer = 0.7f;
-	float barLength = 0.0f;
 }
