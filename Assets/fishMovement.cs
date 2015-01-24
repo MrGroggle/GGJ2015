156357336bb5a981c8cfc3b15e5189ef42166288
Alex <alex_jamesclark@hotmail.co.uk>
2015-01-24 17:22:19 +0000

    Added pickup, added fish
    Fish move and animate
    Can pickup items to the inventory

---
+++ Assets/fishMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class fishMovement : MonoBehaviour {
+
+	public  List<Transform> target;
+	public bool isMoving = false;
+	public float speed = 5.0f;
+	private Transform newTarget;
+
+
+	void Start()
+	{
+	animation.Play("Swim");
+	animation["Swim"].wrapMode = WrapMode.Loop;
+	animation["Swim"].speed = 2;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (isMoving == false) 
+		{
+			newTarget = target[Random.Range(0,target.Count)];
+			isMoving = true;
+
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, newTarget.position, speed * Time.deltaTime);
+		transform.LookAt(newTarget);
+
+
+		if(transform.position == newTarget.position)
+		   {
+			isMoving = false;
+		   }
+
+
+
+	
+	}
+}
---
+++ Assets/itemCollect.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemCollect : MonoBehaviour {
+	private Inv inventory;
+	private bool guiShow = false;
+	public GameObject bush;
+	public GameObject player;
+	private float rayLength = 10f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		inventory = GameObject.Find ("First Person Controller").GetComponent<Inv>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RaycastHit hit = new RaycastHit();
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+
+		if (Physics.Raycast (transform.position, fwd, out hit, rayLength)) {
+			Debug.Log ("hit!");
+						if (hit.collider.gameObject.tag == "fish") {
+				Debug.Log ("hit fish!");
+						guiShow = true;
+
+								if (Input.GetKeyDown (KeyCode.E)) {
+
+										inventory.fish++;
+										Destroy (hit.collider.gameObject);
+										guiShow = false;
+								}
+
+						} 
+				}
+		else 
+		{
+			guiShow = false;
+			}
+
+	
+	}
+
+	void OnGUI()
+	{
+		if(guiShow == true)
+		{
+			GUI.Box(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 150, 100, 20), "PICKUP!");
+		}
+	}
+
+
+}
