3ec67ad4fe6fad61cc52a01ad45e56387ba6f2ab
Dezachu <Dezachu@hotmail.com>
2015-01-24 19:34:04 +0000

    FishCook script initial

---
+++ Assets/Scripts/FishCook.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishCook : MonoBehaviour 
+{
+	private GameObject Player;
+	
+	void Start () 
+	{
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update () 
+	{
+		RaycastHit hit = new RaycastHit();
+		Vector3 fwd = Player.transform.TransformDirection (Vector3.forward);
+
+		//if (RaycastHit(Physics.Raycast (Player.transform.position, fwd, out hit, 3f)))
+		//{
+		//	Debug.Log (Time.realtimeSinceStartup.ToString() + ": Can cook");
+		//}
+	}
+}
