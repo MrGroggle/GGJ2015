83dd958a5a66f0a9c42df64b3f6df4b84abe23cd
Dezachu <Dezachu@hotmail.com>
2015-01-25 12:14:27 +0000

    Can cook fish!
    To get in:
    - Add "FishCook" script to FPSController
    - Add "Campfire" tag to "Inst_Campfire" prefab (In Assets folder)
    - Add BoxCollider to Inst_Campfire with values 0, 0.75, 0, 1.5, 1.5, 1.5

--- Assets/Scripts/FishCook.cs
+++ Assets/Scripts/FishCook.cs
@@ -4,20 +4,59 @@ using System.Collections;
 public class FishCook : MonoBehaviour 
 {
 	private GameObject Player;
+	private bool		GUIShow;
+	
+	private float		CookedTimer;
+	private float 		MessageTimer = 1f;
 	
 	void Start () 
 	{
 		Player = GameObject.FindGameObjectWithTag ("Player");
+		GUIShow = false;
 	}
 
 	void Update () 
 	{
 		RaycastHit hit = new RaycastHit();
 		Vector3 fwd = Player.transform.TransformDirection (Vector3.forward);
+		GUIShow = false;
 
-		//if (RaycastHit(Physics.Raycast (Player.transform.position, fwd, out hit, 3f)))
-		//{
-		//	Debug.Log (Time.realtimeSinceStartup.ToString() + ": Can cook");
-		//}
+		CookedTimer -= Time.deltaTime;
+
+		if (Physics.Raycast (transform.position, fwd, out hit, 5f))
+		{
+			if (hit.collider.gameObject.tag == "Campfire")
+			{
+				Debug.DrawLine(Player.transform.position, hit.point);
+				GUIShow = true;
+			}
+		}
+
+		if (GUIShow && Input.GetKeyDown("e"))
+		{
+			if (Player.GetComponent<Inv>().fish > 0)
+			{
+				Player.GetComponent<Inv>().fish -= 1;
+				Player.GetComponent<Inv>().food += 1;
+
+				CookedTimer = MessageTimer;
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		if(GUIShow == true)
+		{
+			float RectWidth = Screen.width * 0.3f;
+			float RectHeight = Screen.height * 0.06f;
+
+			GUI.Box(new Rect(Screen.width / 2f - (RectWidth * 0.5f), Screen.height * 0.25f - (RectHeight * 0.5f), RectWidth, RectHeight), Player.GetComponent<Inv>().fish == 0 ? "You have no fish to cook!" : "Press 'E' to cook a fish");
+
+			if (CookedTimer > 0f)
+			{
+				GUI.Box(new Rect(Screen.width / 2f - (RectWidth * 0.5f), Screen.height * 0.35f - (RectHeight * 0.5f), RectWidth, RectHeight), "Fish cooked!");
+			}
+		}
 	}
 }
