b8177e2bc57fad92b2cd630ebf68b56521f85259
megamareep <delltom89@gmail.com>
2015-01-25 14:08:55 +0000

    Major Changes

---
+++ Assets/MazeScene/CompletedLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class CompletedLevel : MonoBehaviour {
+
+
+	public string Level;
+	void OnTriggerEnter(Collider other) 
+	{
+		Application.LoadLevel (Level);
+	}
+}
