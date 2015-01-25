2918aeb208037b1f4bb16d30a83ef3d8f102e31e
Alex <alex_jamesclark@hotmail.co.uk>
2015-01-25 11:16:25 +0000

    Added Weapon Swapping
    Put in pickaxe and axe
    Press Q to swap

---
+++ Assets/Scripts/itemSwitching.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemSwitching : MonoBehaviour {
+	public GameObject weapon1;
+	public GameObject weapon2;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+		    {
+			SwapWeapons();
+			}
+
+
+	}
+
+	
+	void SwapWeapons()
+	{
+		if (weapon1.active == true) {
+						weapon1.SetActiveRecursively (false);
+						weapon2.SetActiveRecursively (true);
+
+
+				} else {
+						weapon1.SetActiveRecursively (true);
+						weapon2.SetActiveRecursively (false);
+				}
+			
+			
+	}
+
+
+}
+
