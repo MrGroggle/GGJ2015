0d418eb5d109bd6521f134b28dbe6574679b2dfa
megamareep <delltom89@gmail.com>
2015-01-25 13:44:10 +0000

    CABIN ADDED
    PLAYER IS NOW PREFABBED

---
+++ Assets/Cabin/CabinSticktoFloor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CabinSticktoFloor : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		if (collision.gameObject.tag == "Terrain") {
+						this.gameObject.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+				}
+
+	}
+
+}
